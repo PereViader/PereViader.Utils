8ea594c7caa88db1fc3ff4156ec3e0d143a38058
Pere Viader <pere.viader22@gmail.com>
2023-11-22 21:54:58 +0100

    Add optional and it's extensions

---
+++ PereViader.Utils.Common/PereViader.Utils.Common/Optional/Optional.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PereViader.Utils.Common.Optional
+{
+    public readonly struct Optional<T>
+    {
+        private readonly T _value;
+        public bool HasValue { get; }
+
+        private Optional(T value, bool hasValue)
+        {
+            _value = value;
+            HasValue = hasValue;
+        }
+
+        public static Optional<T> Some(T value)
+        {
+            if (value == null)
+            {
+                return None;
+            }
+            
+            return new Optional<T>(value, true);
+        }
+        
+        public static Optional<T> Maybe(T value, bool hasValue)
+        {
+            if (!hasValue)
+            {
+                return Optional<T>.None;
+            }
+            
+            return Some(value);
+        }
+
+        public static Optional<T> None => default;
+
+        public bool TryGetValue(out T value)
+        {
+            value = _value;
+            return HasValue;
+        }
+        
+        public T UnsafeGetValue()
+        {
+            if (!HasValue)
+            {
+                throw new InvalidOperationException($"No value present in {nameof(Optional<T>)}");
+            }
+            
+            return _value;
+        }
+    }
+}
---
+++ PereViader.Utils.Common/PereViader.Utils.Common/Optional/OptionalExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace PereViader.Utils.Common.Optional
+{
+    public static class OptionalExtensions
+    {
+        public static TResult Match<T, TResult>(this Optional<T> optional, Func<T, TResult> whenSome, Func<TResult> whenNone)
+        {
+            if (optional.TryGetValue(out var value))
+            {
+                return whenSome(value);
+            }
+            
+            return whenNone();
+        }
+        
+        public static void Match<T, TResult>(this Optional<T> optional, Action<T> whenSome, Action whenNone)
+        {
+            if (optional.TryGetValue(out var value))
+            {
+                whenSome(value);
+                return;
+            }
+            
+            whenNone();
+        }
+
+        public static Optional<TResult> MapSome<T, TResult>(this Optional<T> optional, Func<T, TResult> func)
+        {
+            return optional.TryGetValue(out var value) 
+                ? Optional<TResult>.Some(func(value)) 
+                : Optional<TResult>.None;
+        }
+        
+        public static Optional<TResult> MapSome<T, TResult>(this Optional<T> optional, Func<T, Optional<TResult>> func)
+        {
+            return optional.TryGetValue(out var value) 
+                ? func(value) 
+                : Optional<TResult>.None;
+        }
+
+        public static Optional<T> MapNone<T>(this Optional<T> optional, Func<T> func)
+        {
+            return optional.HasValue 
+                ? optional 
+                : Optional<T>.Some(func.Invoke());
+        }
+        
+        public static Optional<T> MapNone<T>(this Optional<T> optional, Func<Optional<T>> func)
+        {
+            return optional.HasValue 
+                ? optional 
+                : func.Invoke();
+        }
+        
+        public static T GetValueOrDefault<T>(this Optional<T> optional, T defaulValue = default)
+        {
+            if (optional.TryGetValue(out var value))
+            {
+                return value;
+            }
+            
+            return defaulValue;
+        }
+        
+        public static T GetValueOrDefault<T>(this Optional<T> optional, Func<T> func)
+        {
+            if (optional.TryGetValue(out var value))
+            {
+                return value;
+            }
+            
+            return func();
+        }
+    }
+}
