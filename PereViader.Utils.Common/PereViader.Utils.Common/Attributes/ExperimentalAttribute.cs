82718405dc0f4f8014ade4cc5776b2d852235e70
Pere Viader <pere.viader22@gmail.com>
2023-11-27 00:17:34 +0100

    Add ExperimentalAttribute Useful when you want to tell that some piece of functionality is still not ready to be used or that can be used with caveats

---
+++ PereViader.Utils.Common/PereViader.Utils.Common/Attributes/ExperimentalAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PereViader.Utils.Common.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.All, AllowMultiple = false, Inherited = true)]
+    public class ExperimentalAttribute : Attribute
+    {
+    }
+}
