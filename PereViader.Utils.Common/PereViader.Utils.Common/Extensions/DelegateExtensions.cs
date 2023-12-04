21b6aa98c206deaf0fcd8c2415b0bebdeda1364e
Pere Viader <pere.viader22@gmail.com>
2023-12-04 18:31:55 +0100

    chore: Rename names of cached delegates

--- PereViader.Utils.Common/PereViader.Utils.Common/Extensions/DelegateExtensions.cs
+++ PereViader.Utils.Common/PereViader.Utils.Common/Extensions/DelegateExtensions.cs
@@ -8,19 +8,19 @@ namespace PereViader.Utils.Common.Extensions
         public static class With
         {
             public static readonly Action DoNothingAction = () => { };
-            public static readonly Func<bool> ReturnTrueFunc = () => true;
-            public static readonly Func<bool> ReturnFalseFunc = () => true;
-            public static readonly Func<Task> ReturnCompletedTaskFunc = () => Task.CompletedTask;
+            public static readonly Func<bool> TrueFunc = () => true;
+            public static readonly Func<bool> FalseFunc = () => true;
+            public static readonly Func<Task> CompletedTaskFunc = () => Task.CompletedTask;
         }
 
         public static class With<TArg1>
         {
             public static readonly Action<TArg1> DoNothingAction = (arg1) => { };
-            public static readonly Predicate<TArg1> ReturnTruePredicate = (arg1) => true;
-            public static readonly Predicate<TArg1> ReturnFalsePredicate = (arg1) => false;
-            public static readonly Func<TArg1, bool> ReturnTrueFunc = (arg1) => true;
-            public static readonly Func<TArg1, bool> ReturnFalseFunc = (arg1) => false;
-            public static readonly Func<TArg1, Task> ReturnCompletedTaskWithArg1Func = (arg1) => Task.FromResult(arg1);
+            public static readonly Predicate<TArg1> TruePredicate = (arg1) => true;
+            public static readonly Predicate<TArg1> FalsePredicate = (arg1) => false;
+            public static readonly Func<TArg1, bool> TrueFunc = (arg1) => true;
+            public static readonly Func<TArg1, bool> FalseFunc = (arg1) => false;
+            public static readonly Func<TArg1, Task> CompletedTaskWithArg1Func = (arg1) => Task.FromResult(arg1);
             
             public static Predicate<TArg1> TrueAfterNCallsPredicate(int n)
             {
@@ -40,10 +40,10 @@ namespace PereViader.Utils.Common.Extensions
         public static class With<TArg1, TArg2>
         {
             public static readonly Action<TArg1, TArg2> DoNothingAction = (arg1, arg2) => { };
-            public static readonly Func<TArg1,TArg2, bool> ReturnTrueFunc = (arg1, arg2) => true;
-            public static readonly Func<TArg1, TArg2, bool> ReturnFalseFunc = (arg1, arg2) => false;
-            public static readonly Func<TArg1, TArg2, Task> ReturnCompletedTaskWithArg1Func = (arg1, arg2) => Task.FromResult(arg1);
-            public static readonly Func<TArg1, TArg2, Task> ReturnCompletedTaskWithArg2Func = (arg1, arg2) => Task.FromResult(arg2);
+            public static readonly Func<TArg1,TArg2, bool> TrueFunc = (arg1, arg2) => true;
+            public static readonly Func<TArg1, TArg2, bool> FalseFunc = (arg1, arg2) => false;
+            public static readonly Func<TArg1, TArg2, Task> CompletedTaskWithArg1Func = (arg1, arg2) => Task.FromResult(arg1);
+            public static readonly Func<TArg1, TArg2, Task> CompletedTaskWithArg2Func = (arg1, arg2) => Task.FromResult(arg2);
         }
     }
 }
