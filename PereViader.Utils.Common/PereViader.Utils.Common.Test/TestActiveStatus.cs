2e0348a1942b82890f6d391cc237687d4c54903f
Pere Viader <pere.viader22@gmail.com>
2023-12-05 15:21:58 +0100

    feat: Add ActiveStatus ActiveStatus is a class that allows generic tracking of active state of any object.

---
+++ PereViader.Utils.Common/PereViader.Utils.Common.Test/TestActiveStatus.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using PereViader.Utils.Common.ActiveStatus;
+
+namespace PereViader.Utils.Common.Test;
+
+[TestFixture]
+public class TestActiveStatus
+{
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void IsActive_OnElementWithNoCallsAndDefaultTrue_IsTheDefault(bool defaultState)
+    {
+        var activeStatus = new ActiveStatus<int>(defaultState);
+            
+        Assert.That(activeStatus.IsActive(0), Is.EqualTo(defaultState));
+    }
+        
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void IsActive_OnElementThatWasRegisteredAsTheContrary_IsTheContrary(bool defaultState)
+    {
+        var activeStatus = new ActiveStatus<int>(defaultState);
+
+        activeStatus.UpdateStatus(new object(), 0, !defaultState);
+
+        Assert.That(activeStatus.IsActive(0), Is.EqualTo(!defaultState));
+    }
+        
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void IsActive_AfterForgetting_IsResetToDefault(bool defaultState)
+    {
+        var activeStatus = new ActiveStatus<int>(defaultState);
+
+        activeStatus.UpdateStatus(new object(), 0, !defaultState);
+        activeStatus.Forget(0);
+
+        Assert.That(activeStatus.IsActive(0), Is.EqualTo(defaultState));
+    }
+        
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void IsActive_AfterAddingSomeOwnersWithDefaultStateAndOthersWithContrary_IsUpdatedProperly(bool defaultState)
+    {
+        var activeStatus = new ActiveStatus<int>(defaultState);
+
+        activeStatus.UpdateStatus(new object(), 0, defaultState);
+        activeStatus.UpdateStatus(new object(), 0, !defaultState);
+        activeStatus.UpdateStatus(new object(), 0, defaultState);
+            
+        Assert.That(activeStatus.IsActive(0), Is.EqualTo(!defaultState));
+    }
+        
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void UpdateStatus_ReturnsTrueWhenItUpdatesItAndFalseWhenNot(bool defaultState)
+    {
+        var activeStatus = new ActiveStatus<int>(defaultState);
+
+        var update1 = activeStatus.UpdateStatus(new object(), 0, defaultState);
+        var update2 = activeStatus.UpdateStatus(new object(), 0, !defaultState);
+        var update3 = activeStatus.UpdateStatus(new object(), 0, !defaultState);
+        var update4 = activeStatus.UpdateStatus(new object(), 0, defaultState);
+
+        Assert.That(update1, Is.EqualTo(false));
+        Assert.That(update2, Is.EqualTo(true));
+        Assert.That(update3, Is.EqualTo(false));
+        Assert.That(update4, Is.EqualTo(false));
+    }
+}
---
+++ PereViader.Utils.Common/PereViader.Utils.Common/ActiveStatus/ActiveStatus.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PereViader.Utils.Common.ActiveStatus
+{
+    public sealed class ActiveStatus<TId>
+    {
+        public event Action<TId, bool> OnChanged;
+
+        private readonly Dictionary<TId, HashSet<object>> _activeStatuses = new Dictionary<TId, HashSet<object>>();
+
+        public bool DefaultActiveState { get; }
+
+        public ActiveStatus(bool defaultActiveState = false)
+        {
+            DefaultActiveState = defaultActiveState;
+        }
+
+        public bool IsActive(TId id)
+        {
+            if (!_activeStatuses.TryGetValue(id, out var registry))
+            {
+                return DefaultActiveState;
+            }
+            
+            return (registry.Count > 0) ^ DefaultActiveState;
+        }
+
+        public bool UpdateStatus(object owner, TId id, bool active)
+        {
+            var previousActive = IsActive(id);
+            
+            if (!_activeStatuses.TryGetValue(id, out var registry))
+            {
+                registry = new HashSet<object>();
+                _activeStatuses.Add(id, registry);
+            }
+            
+            if (active != DefaultActiveState)
+            {
+                registry.Add(owner);
+            }
+            else
+            {
+                registry.Remove(owner);
+            }
+
+            var currentIsActive = IsActive(id);
+            var hasChanged = previousActive != currentIsActive;
+
+            if (hasChanged)
+            {
+                OnChanged?.Invoke(id, currentIsActive);
+            }
+            
+            return hasChanged;
+        }
+
+        public void Forget(TId id)
+        {
+            _activeStatuses.Remove(id);
+        }
+
+        public void ForgetAll()
+        {
+            _activeStatuses.Clear();
+        }
+    }
+}
