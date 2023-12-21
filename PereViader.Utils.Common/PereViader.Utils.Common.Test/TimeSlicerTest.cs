7be6c20e3c580843ef6c8ef9b028c2d46c522f04
Pere Viader <pere.viader22@gmail.com>
2023-12-21 23:30:23 +0100

    feat: Add time slicing utility for tasks Very useful for loading screens and initializaiton processes that must happen in split over several frames

---
+++ PereViader.Utils.Common/PereViader.Utils.Common.Test/TimeSlicerTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using PereViader.Utils.Common.TimeSlicing;
+
+namespace PereViader.Utils.Common.Test;
+
+[TestFixture]
+public class TimeSlicerTest
+{
+    [Test]
+    public void SliceAsync_OnNew_DoesNotSlice()
+    {
+        var dateTime = new DateTime(2000, 1, 1, 1, 1, 1);
+        var slicer = new TimeSlicer(TimeSpan.FromSeconds(1), () => dateTime);
+
+        var willSlice = slicer.WillSlice();
+        var task = slicer.SliceAsync();
+        
+        Assert.That(willSlice, Is.False);
+        Assert.That(task.IsCompleted, Is.True);
+    }
+    
+    [Test]
+    public void SliceAsync_AfterTime_DoesSliceOnce()
+    {
+        var dateTime = new DateTime(2000, 1, 1, 1, 1, 1);
+        var slicer = new TimeSlicer(TimeSpan.FromSeconds(1), () => dateTime);
+
+        dateTime += TimeSpan.FromSeconds(2);
+        var willSlice1 = slicer.WillSlice();
+        var task1 = slicer.SliceAsync();
+        var willSlice2 = slicer.WillSlice();
+        var task2 = slicer.SliceAsync();
+
+        Assert.That(willSlice1, Is.True);
+        Assert.That(task1.IsCompleted, Is.False);
+        Assert.That(willSlice2, Is.False);
+        Assert.That(task2.IsCompleted, Is.True);
+    }
+    
+    [Test]
+    public void SliceAsync_AfterReset_DoesNotSlice()
+    {
+        var dateTime = new DateTime(2000, 1, 1, 1, 1, 1);
+        var slicer = new TimeSlicer(TimeSpan.FromSeconds(1), () => dateTime);
+
+        dateTime += TimeSpan.FromSeconds(2);
+        slicer.Reset();
+        var willSlice = slicer.WillSlice();
+        var task = slicer.SliceAsync();
+
+        Assert.That(willSlice, Is.False);
+        Assert.That(task.IsCompleted, Is.True);
+    }
+}
---
+++ PereViader.Utils.Common/PereViader.Utils.Common/TimeSlicing/TimeSlicer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PereViader.Utils.Common.TimeSlicing
+{
+    public sealed class TimeSlicer
+    {
+        private readonly Func<DateTime> _currentDateTimeFunc;
+        
+        public TimeSpan TimeSpanBetweenSlices { get; set; }
+        public DateTime DateTimeOnPreviousSlice { get; private set; }
+        
+
+        public TimeSlicer(TimeSpan timeSpanBetweenSlices, Func<DateTime> currentDateTimeFunc)
+        {
+            TimeSpanBetweenSlices = timeSpanBetweenSlices;
+            _currentDateTimeFunc = currentDateTimeFunc;
+            DateTimeOnPreviousSlice = currentDateTimeFunc();
+        }
+
+        public bool WillSlice()
+        {
+            DateTime now = _currentDateTimeFunc();
+            return now - DateTimeOnPreviousSlice >= TimeSpanBetweenSlices;
+        }
+
+        public bool Slice()
+        {
+            DateTime now = _currentDateTimeFunc();
+            if (now - DateTimeOnPreviousSlice <= TimeSpanBetweenSlices)
+            {
+                return false;
+            }
+
+            DateTimeOnPreviousSlice = _currentDateTimeFunc();
+            return true;
+        }
+
+        public async ValueTask SliceAsync()
+        {
+            if (Slice())
+            {
+                await Task.Yield();
+            }
+        }
+
+        public void Reset()
+        {
+            DateTimeOnPreviousSlice = _currentDateTimeFunc();
+        }
+    }
+}
