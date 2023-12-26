06d6365479ddf685f69383b0e54d9daa0a21a2f6
Pere Viader <pere.viader22@gmail.com>
2023-12-26 21:33:54 +0100

    feat: Add RunAsync on UnityTaskExtensions.cs

---
+++ PereViader.Utils.Unity3d/Assets/PereViader.Utils.Unity3d/Scripts/Runtime/Extensions/UnityTaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace PereViader.Utils.Unity3d.Extensions
+{
+    public static class UnityTaskExtensions
+    {
+        public static async void RunAsync(this Task task)
+        {
+            await task;
+        }
+    }
+}
