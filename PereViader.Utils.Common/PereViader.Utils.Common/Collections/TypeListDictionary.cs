4d5df180b23cc84b0ad98974afa14de4fe144bb1
Pere Viader <pere.viader22@gmail.com>
2024-01-05 01:12:09 +0100

    feat: Update TypeListDictionary to follow the same assumptions as TypeDictionary

--- PereViader.Utils.Common/PereViader.Utils.Common/Collections/TypeListDictionary.cs
+++ PereViader.Utils.Common/PereViader.Utils.Common/Collections/TypeListDictionary.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using PereViader.Utils.Common.Attributes;
+using PereViader.Utils.Common.Extensions;
 
 namespace PereViader.Utils.Common.Collections
 {
@@ -11,37 +12,27 @@ namespace PereViader.Utils.Common.Collections
     [Experimental]
     public sealed class TypeListDictionary : Dictionary<Type, object>
     {
-        /// <summary>
-        /// Adds a new element with the provided value to the dictionary.
-        /// The type of the provided value is used as a key.
-        /// </summary>
-        /// <typeparam name="T">The type of the value to be added.</typeparam>
-        /// <param name="value">The value to add to the dictionary.</param>
-        public void Add<T>(T value)
+        public bool ContainsKey<T>() => ContainsKey(typeof(T));
+        
+        public void AddValue<T>(T value)
         {
-            if (!TryGetValuesInternal<T>(out var list))
+            if (!TryGetValues<T>(out var list))
             {
                 list = new List<T>();
                 this[typeof(T)] = list;
             }
 
-            list!.Add(value);
+            list.Add(value);
         }
-
-        /// <summary>
-        /// Removes an element with the provided value from the dictionary.
-        /// The type of the provided value is used as a key.
-        /// </summary>
-        /// <typeparam name="T">The type of the value to be removed.</typeparam>
-        /// <param name="value">The value to remove from the dictionary.</param>
+        
         public bool Remove<T>(T value)
         {
-            if (!TryGetValuesInternal<T>(out var list))
+            if (!TryGetValues<T>(out var list))
             {
                 return false;
             }
 
-            var removed = list!.Remove(value);
+            var removed = list.Remove(value);
             
             if (list.Count == 0)
             {
@@ -51,64 +42,20 @@ namespace PereViader.Utils.Common.Collections
             return removed;
         }
 
-        /// <summary>
-        /// Retrieves the value associated with the given type key.
-        /// </summary>
-        /// <typeparam name="T">The type of the value to retrieve.</typeparam>
-        /// <returns>The value associated with the given type key.</returns>
-        /// <exception cref="KeyNotFoundException">Thrown when the key is not found in the dictionary.</exception>
-        public IReadOnlyList<T> GetValues<T>()
-        {
-            if (!TryGetValues<T>(out var value))
-            {
-                throw new KeyNotFoundException($"The key {typeof(T).FullName} was not found in the TypeDictionary");
-            }
+        public List<T>? GetValues<T>() => TryGetValues<T>(out var values).ToNullable(values!);
 
-            return value!;
-        }
-
-        /// <summary>
-        /// Tries to get the value associated with the specified type key.
-        /// </summary>
-        /// <typeparam name="T">The type of the value to get.</typeparam>
-        /// <param name="values">When this method returns, contains the value associated with the specified key, if the key is found;
-        /// otherwise, the default value for the type of the value parameter.</param>
-        /// <returns>true if the TypeDictionary contains an element with the specified key; otherwise, false.</returns>
-        public bool TryGetValues<T>(out IReadOnlyList<T>? values)
-        {
-            if (!TryGetValuesInternal<T>(out var listValues))
-            {
-                values = default;
-                return false;
-            }
-
-            values = listValues;
-            return true;
-        }
-
-        bool TryGetValuesInternal<T>(out List<T>? values)
+        public bool TryGetValues<T>(out List<T> values)
         {
             var type = typeof(T);
             
-            if (!TryGetValue(type, out object? objectList))
+            if (!TryGetValue(type, out var objectList))
             {
-                values = default;
+                values = default!;
                 return false;
             }
 
             values = (List<T>)objectList;
             return true;
-        }
-
-        /// <summary>
-        /// Determines whether the TypeDictionary contains the specified type key.
-        /// </summary>
-        /// <typeparam name="T">The type of the key to locate in the TypeDictionary.</typeparam>
-        /// <returns>true if the TypeDictionary contains an element with the specified key; otherwise, false.</returns>
-        public bool ContainsKey<T>()
-        {
-            var type = typeof(T);
-            return ContainsKey(type);
         }
     }
 }
