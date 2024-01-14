22c9e04e69d75fd2c60449a5e2b23dbfdcfb0892
Pere Viader <pere.viader22@gmail.com>
2024-01-14 16:55:38 +0100

    feat: Add navigation components and sample

---
+++ PereViader.Utils.Unity3d/Assets/PereViader.Utils.Unity3d/Scripts/Runtime/UI/TMPInputFieldButton.cs
@@ -0,0 +1,65 @@
+﻿using PereViader.Utils.Unity3d.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PereViader.Utils.Unity3d.UI
+{
+    public class TMPInputFieldButton : MonoBehaviour
+    {
+        [Tooltip("If null, will create the button component on Awake")]
+        public Button Button;
+        public TMP_InputField InputField;
+
+        public void Awake()
+        {
+            if (Button == null)
+            {
+                Button = gameObject.GetOrAddComponent<Button>();
+            }
+            Button.onClick ??= new Button.ButtonClickedEvent();
+            Button.onClick.AddListener(StartEditing);
+
+            InputField.onEndEdit ??= new TMP_InputField.SubmitEvent();
+            InputField.onEndEdit.AddListener(EndEditing);
+            
+            Button.transition = InputField.transition;
+            Button.targetGraphic = InputField.targetGraphic;
+            Button.colors = InputField.colors;
+            Button.spriteState = InputField.spriteState;
+            Button.animationTriggers = InputField.animationTriggers;
+        }
+
+        private void StartEditing()
+        {
+            InputField.enabled = true;
+            InputField.Select();
+            Button.enabled = false;
+        }
+        
+        private void EndEditing(string text)
+        {
+            Button.enabled = true;
+            InputField.enabled = false;
+
+            StartCoroutine(CoroutineExtensions.WaitFrame(x =>
+            {
+                if (EventSystem.current.currentSelectedGameObject == x.InputField.gameObject)
+                {
+                    x.Button.Select();
+                }
+            }, (Button, InputField)));
+        }
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            if (InputField == null)
+            {
+                InputField = GetComponentInChildren<TMP_InputField>();
+            }
+        }
+#endif
+    }
+}
---
+++ PereViader.Utils.Unity3d/Assets/PereViader.Utils.Unity3d/Scripts/Runtime/UI/UpdateScrollRectOnSelected.cs
@@ -0,0 +1,43 @@
+﻿using PereViader.Utils.Unity3d.Extensions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PereViader.Utils.Unity3d.UI
+{
+    public class UpdateScrollRectOnSelected : MonoBehaviour, ISelectHandler
+    {
+        [Tooltip("Can be null, will use self")]
+        public RectTransform RectTransform;
+        public Vector2 NormalizedViewportPosition = Vector2Extensions.Half;
+
+        public void Awake()
+        {
+            if (RectTransform == null)
+            {
+                RectTransform = this.UnsafeGetRectTransform();
+            }
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            if (eventData is not AxisEventData)
+            {
+                return;
+            }
+            
+            var scrollRect = GetComponentInParent<ScrollRect>();
+            if (scrollRect == null)
+            {
+                Debug.LogError("There was no ScrollRect to scroll", this);
+            }
+
+            scrollRect.normalizedPosition = scrollRect.GetElementNormalizedPosition(RectTransform, NormalizedViewportPosition);        
+        }
+
+        private void Reset()
+        {
+            RectTransform = this.UnsafeGetRectTransform();
+        }
+    }
+}
