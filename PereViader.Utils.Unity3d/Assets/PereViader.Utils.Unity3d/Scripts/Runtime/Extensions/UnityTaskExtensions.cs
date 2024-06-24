51d16132f8490510fe8ff0bd3e64b6e4d69e2a79
Pere Viader <pere.viader22@gmail.com>
2024-06-24 11:04:38 +0200

    Add more tasks to UnityTaskExtensions.cs

--- PereViader.Utils.Unity3d/Assets/PereViader.Utils.Unity3d/Scripts/Runtime/Extensions/UnityTaskExtensions.cs
+++ PereViader.Utils.Unity3d/Assets/PereViader.Utils.Unity3d/Scripts/Runtime/Extensions/UnityTaskExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PereViader.Utils.Unity3d.Extensions
 {
@@ -6,7 +8,46 @@ namespace PereViader.Utils.Unity3d.Extensions
     {
         public static async void RunAsync(this Task task)
         {
-            await task;
+            try
+            {
+                await task;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+        
+        public static async Task WaitUntil(this Func<bool> func, CancellationToken ct)
+        {
+            while (!func())
+            {
+                await Task.Yield();
+                ct.ThrowIfCancellationRequested();
+            }
+        }
+        
+        public static async Task WaitWhile(this Func<bool> func, CancellationToken ct)
+        {
+            while (func())
+            {
+                await Task.Yield();
+                ct.ThrowIfCancellationRequested();
+            }
+        }
+        
+        public static async Task WaitFrame(CancellationToken ct)
+        {
+            await Task.Yield();
+            ct.ThrowIfCancellationRequested();
+        }
+        
+        public static async Task WaitFrames(int frameCount, CancellationToken ct)
+        {
+            for (int i = 0; i < frameCount; i++)
+            {
+                await Task.Yield();
+                ct.ThrowIfCancellationRequested();
+            }
         }
     }
 }
